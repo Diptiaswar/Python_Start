e3cd81f2fe5e85b9f88705a965bc739704764f84
Diptiaswar <87604922+Diptiaswar@users.noreply.github.com>
2021-07-19 17:22:39 +0530

    Add files via upload

---
+++ # program code to convert Kilometer to M.py
@@ -0,0 +1,10 @@
+# program code to convert Kilometer to Miles
+km = int(input(" Enter the value in Kilometer"))
+
+# 1 Km = 0.621371 Mil
+ratio = 0.621371 
+
+# Converting km to Mil
+mi= km * ratio
+
+print("The value after converting into miles:",mi)
